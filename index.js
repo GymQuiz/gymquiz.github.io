34709e66fcddb5dd12e242f4c7763f139b2b5908
Florin <florin.bucherer@gmail.com>
2022-09-15 16:05:41 +0200

    First working Quiz from GymQuiz Beta

---
+++ index.js
@@ -0,0 +1,194 @@
+const fullVociDE =["die Reise", "reisen", "abfahren", "abreisen, verreisen", "der Urlaub", "die Ferien", "das Ferienlager", "grossen Ferien", "der Tourismus", "die/der TouristIn", "touristisch", "Gute Reise!", "die Rückkehr, der Rückweg", "zurückkehren", "nach Hause kommen", "zurückkommen", "das Reisebüro", "buchen", "stornieren", "das Gepäck", "der Koffer", "packen", "die Papiere", "der Ausweis", "der Personalausweis", "der Reisepass", "gültig", "ungültig", "der Aufenthalt", "die Erholung", "erholsam", "organisieren", "die Bestätigung", "bestätigen", "der Urlaub auf dem Bauernhof", "das Wellnesshotel", "das Abenteuer", "der Rucksack", "die Reisetasche", "auf der Durchreise sein", "im Ausland", "das Visum", "der Zoll", "verzollen", "das Hotel", "die Jugendherberge", "das Camping/Campingplatz", "zelten", "das Zelt", "der Schlafsack", "die Rezeption", "das Doppelzimmer", "das Einzelzimmer", "belegt, ausgebucht", "die Vollpension", "die Halbpension", "die Ferienwohnung", "die Saison", "die Ausrüstung", "die Sehenswürdigkeit", "die Besichtigung", "besichtigen", "der Ausflug", "der Reiseführer", "die Führung", "die Rundfahrt", "die Kirche", "die Kathedrale", "die Basilika", "die Synagoge", "die Moschee", "das Museum", "der Saal", "der Turm", "das Schloss", "der Palast", "der Eintritt", "die Kultur", "kulturell", "die Kapelle", "der Kirchturm", "die Glocke", "die Kuppel", "die Burg", "die Ruine", "der Tempel", "die Säule", "der Brunnen", "das Denkmal", "das Wahrzeichen", "die Sicht/Aussicht", "die Wegbeschreibung", "der Ort", "die Stelle", "das Land", "die Stadt", "das Stadtzentrum", "das Dorf", "der Vorort", "das Viertel/Stadtteil", "das Gebäude", "die Kreuzung", "der Platz", "die Strasse", "die Landstrasse", "die Avenue", "der Boulevard", "die Autobahn", "die Ausfahrt", "der Parkplatz", "der Gehweg", "die Brücke", "der Park", "der Friedhof", "der Stadtplan", "die Karte", "das Rathaus", "der Bus", "die Strassenbahn", "die Haltestelle", "die Fahrkarte", "umsteigen", "die Verspätung", "sich verspäten", "warten (auf)", "die öffentlichen Verkehrsmittel", "die Hauptverkehrszeit", "der Busbahnhof", "die Linie", "die Zeitkarte", "der Fahrkartenautomat", "entwerten", "die Endstation", "der Bahnhof", "der Zug", "die Eisenbahn", "Hochgeschwindigkeitszug", "der Regionalzug", "der Regionalexpress", "der Fahrplan/Flugplan", "die Abreise", "die Ankunft", "verpassen", "verpassen", "verpassen", "die Fahrkarte", "die Auskunft", "der Anschluss", "die Hinfahrt", "die Rückfahrt", "die Fahrkarte für eine einfache Fahrt", "die Rückfahrkarte", "durchgehend", "der Sitzplatz", "der Bahnsteig", "die Schiene", "der/die Schaffner/in", "der Waggon", "der Speisewagen", "der Schlafwagen", "das Abteil", "das Gleis", "das Bahnhofsrestaurant", "die Gepäckaufbewahrung", "die Schliessfächer", "das Flugzeug", "die Maschine", "der Flug", "der Direktflug", "fliegen", "der/die PassagierIn", "die Fluglinie", "der Flughafen", "der Flugsteig", "das Flugticket", "das E-Ticket", "starten", "landen", "das Boot", "das Schiff", "der Hafen", "die Fähre", "sinken", "der Hubschrauber", "die Zwischenlandung", "abholen", "die Durchsage", "an Bord gehen", "die Kreuzfahrt", "die Kabine", "die Fracht", "beladen", "der Transport", "transportieren", "der Container", "der Verkehr", "das Auto", "das Motorrad", "der Roller", "das Taxi", "der/die FahrerIn", "fahren", "der Führerschein", "das Fahrrad", "das Fahrrad", "mit dem Fahrrad fahren", "die Route", "sich befinden", "vorfahren", "rückwärts fahren", "wenden", "die Ampel", "das Verkehrsschild", "anhalten", "über die Strasse gehen", "parken", "parken", "das Benzin", "der Diesel", "die Tankstelle", "verbrauchen", "der Reifen", "das Fahrzeug", "der/die AutofahrerIn", "der LKW", "der Wohnwagen", "der/die FussgängerIn", "die Abkürzung", "der Umweg", "die Umleitung", "der Stau", "blockieren", "die Kurve", "abbiegen", "die Bremse", "bremsen", "blinken", "das Rad", "das Navi", "eine Panne haben", "die Einbahnstrasse", "das Autokennzeichen", "das Nummernschild", "die Geschwindigkeitsüberschreitung", "der Strafzettel", "der Sicherheitsgurt", "sich anschnaller", "der Sturzhelm", "der Biosprit", "vorgeschrieben", "einparken", "der Rastplatz", "die Maut", "gebührenpflichtig", "das Trampen", "das E-Bike", "trampen"]; //Alle Vocis
+const FullVociEN =["le voyage", "voyager", "partir", "partir en voyage", "le congé", "les vacances", "la colonie (de vacancies)", "grandes vacances", "le tourisme", "le/la touriste", "touristique", "Bon voyage!", "le retour", "retourner", "rentrer", "revenir", "l'agence de voyage", "réserver", "annuler", "les bagages", "la valise", "faire ses valises", "les papiers", "la pièce d'identité", "la carte d'identité", "le passeport", "valide", "perimé périmée", "le séjour", "le repos", "reposant, reposante", "organiser", "la confirmation", "confirmer", "les vacances à la ferme", "le spa", "l'aventure", "le sac à dos", "le sac de voyage", "être de passage", "à l'étranger", "le visa", "la douane", "déclarer", "l'hôtel", "l'auberge de jeunesse", "le camping", "faire du camping", "la tente", "le sac de couchage", "la réception", "la chambre double", "la chambre simple", "complet, complète", "la pension complète", "la demi-pension", "l'appartement pour les vacances", "la saison", "l'équipement", "l'attraction touristique", "la visite", "visiter", "l'excursion", "le guide", "la visite guidée", "le circuit touristique", "l'église", "la cathédrale", "la basilique", "la synagogue", "la mosquée", "le musée", "la salle", "la tour", "le château", "le palais", "l'entrée", "la culture", "culturel, culturelle", "la chapelle", "le clocher", "la cloche", "la coupole", "le château forts, châteaux forts", "la ruine", "le temple", "la colonne", "la fontaine", "le monument", "le symbole", "la vue", "l'itinéraire", "le lieu", "l'endroit", "la campagne", "la ville", "le centre-ville, centres-villes", "le village", "la banlieue", "le quartier", "le bâtiment", "le carrefour", "la place", "la rue", "la route", "l'avenue", "le boulevard", "l'autoroute", "la sortie", "le parking", "le trottoir", "le pont", "le parc", "le cimetière", "le plan de la ville", "la carte", "la mairie", "le bus", "le tram", "l'arrêt", "le ticket", "changer", "le retard", "être en retard", "attendre", "les transports en commun", "les heures de pointe", "la gare routière", "la ligne", "la carte d'abonnement", "le distributeur de tickets", "composter", "le terminus", "la gare", "le train", "le chemin de feur", "le TGV", "le train régional", "le TER", "l'horaire", "le départ", "l'arrivée", "manquer", "rater", "louper", "le billet", "le guichet des renseignements", "la correspondance", "l'aller", "le retour", "l'aller simple", "l'aller et retour", "direct, directe", "la place assise", "le quai", "le rail", "le contrôleur, la contrôleuse", "le wagon", "le wagon-restaurant", "le wagon-lit", "le compartiment", "la voie", "le buffet de la gare", "la consigne", "la consigne automatique", "l'avion", "l'appareil", "le vol", "le vol direct", "voler", "le passager, la passagère", "la compagnie aérienne", "l'aéroport", "la porte d'embarquement", "le billet (d'avion)", "le billet électronique", "décoller", "atterrir", "le barque", "le bateau", "le port", "le ferry", "couler", "l'hélicoptère", "l'escale", "aller chercher", "l'annonce", "embarquer", "la croisière", "la cabine", "la cargaison", "charger", "le transport", "transporter", "le conteneur", "la circulation", "la voiture", "la moto", "le scooter", "le taxi", "le conducteur, la conductrice", "conduire", "le permis de conduire", "la bicyclette", "le vélo", "aller à vélo", "la route", "se trouver", "avancer", "reculer", "faire demi-tour", "le feu", "le panneau", "s'arrêter", "traverser", "garer", "se garer", "l'essence", "le gazole", "la station-service", "consommer", "le pneu", "le véhicule", "l'automobiliste", "le camion", "la caravane", "le piéton, la piétonne", "le raccourci", "le détour", "la déviation", "l'embouteillage", "bloquer", "le virage", "tourner", "le frein", "freiner", "mettre son clignotant", "la roue", "le GPS", "tomber en panne", "le sens unique", "le numéro d'immatriculation", "la plaque d'immatriculation", "l'excès de vitesse", "l'ammende", "la ceinture de sécurité", "attacher se sécurité", "le casque", "le biocarburant", "obligatoire", "faire un créneau", "l'aire (de repos)", "le péage", "à péage", "l'auto-stop", "le VAE", "faire de l'auto-stop"]; 
+let vociDe = fullVociDE;//Vocis, die noch gelernt werden müssen
+let vociEn = FullVociEN;
+let progress = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+let wordIndex; //Variablen für später definieren
+let wordIndexFull;
+let auswahl;
+let ordre;
+let deword;
+let enword;
+let notactive;
+let eingabe;
+
+//Get a random Integer:
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function getRandomIntEx(lengthOfArray,indexToExclude){
+
+  let rand = null;  //an integer
+
+    while(rand === null || rand === indexToExclude){
+       rand = Math.round(Math.random() * (lengthOfArray - 1));
+    }
+
+  return rand;
+}
+
+//Generation of everythin:
+function chooseword(){
+    if (progress[wordIndexFull]==0){ //Wenn der Progress dieses Wortes Null ist:
+      vociDe.splice(wordIndexFull,1); //Wort aus vociDe entfernen
+      vociEn.splice(wordIndexFull,1); //Wort aus vociEn entfernen
+    }
+    ordre = getRandomInt(1,5);
+    document.getElementById("right").style.display="none"; //Richtig Banner ausblenden
+    document.getElementById("wrong").style.display="none"; //Falsch Banner ausblenden
+    wordIndex = getRandomInt(0,vociDe.length); //Index in VociDe des wortes, das abgefragt wird
+    deword = vociDe[wordIndex]; //Deutsches Wort
+    enword = vociEn[wordIndex]; //Englische übersetzung
+    wordIndexFull = fullVociDE.indexOf(deword); //Index des Wort in fullVociDE
+    auswahl = [FullVociEN[getRandomIntEx(fullVociDE.length, wordIndexFull)], FullVociEN[getRandomIntEx(fullVociDE.length, wordIndexFull)], FullVociEN[getRandomIntEx(fullVociDE.length, wordIndexFull)]]; //Die drei falschen zur auswahl stehenden Antwortsmöglichkeiten generieren.    ordre = getRandomInt(1,5);//position der richtigen Antwort
+    notactive = false;//Variable, die verhindert, dass man zweimal Antworten kann -> Hier wird sie auf False gesetzt -> man kann Antwort wählen.
+    if (progress[wordIndexFull]>1){ //Wenn Index grösser als 1-> Multiple Choice
+        document.getElementById("choose").style.display="block";//choose einblenden
+        document.getElementById("write").style.display="none";//write ausblenden
+        document.getElementById("mword").textContent=deword; //Wort auf Webseite schreiben
+        //Buttons beschriften:
+        if (ordre==1){
+        document.getElementById("button1").textContent=enword;
+        document.getElementById("button2").textContent=auswahl[0];
+        document.getElementById("button3").textContent=auswahl[1];
+        document.getElementById("button4").textContent=auswahl[2];
+       }
+   
+       if (ordre==2){
+         document.getElementById("button1").textContent=auswahl[0];
+         document.getElementById("button2").textContent=enword;
+         document.getElementById("button3").textContent=auswahl[1];
+         document.getElementById("button4").textContent=auswahl[2];
+        }
+   
+        if (ordre==3){
+         document.getElementById("button1").textContent=auswahl[0];
+         document.getElementById("button2").textContent=auswahl[1];
+         document.getElementById("button3").textContent=enword;
+         document.getElementById("button4").textContent=auswahl[2];
+        }
+   
+        if (ordre==4){
+         document.getElementById("button1").textContent=auswahl[0];
+         document.getElementById("button2").textContent=auswahl[1];
+         document.getElementById("button3").textContent=auswahl[2];
+         document.getElementById("button4").textContent=enword;
+       }
+      
+    }
+    if (progress[wordIndexFull] == 1){ //Wenn Progress gleich 1 -> Antwort per Tastatureingabe
+      document.getElementById("choose").style.display="none";//choose ausblenden
+      document.getElementById("write").style.display="block";//write einblenden
+      document.getElementById("wword").textContent=deword; //Wort auf Webseite schreiben
+      document.getElementById("write").addEventListener("keypress", function(event){
+        if (event.code==="Enter"){
+          submit();
+        }
+      })
+    }
+    console.log("Richtig: "+ordre)
+}
+//Wird ausgeführt, wenn der richtige Button gedrückt wurde.
+function trueanswer(){
+  notactive=true;//Man kann nicht mehr antworten -> keine zweite Antwort möglich
+  document.getElementById("right").style.display="flex";//Richtig Banner wird eingeblendet
+  progress[wordIndexFull] = progress[wordIndexFull]-1;//Progress um 1 gesenkt
+  document.cookie=JSON.stringify(progress);
+  setTimeout(function() {//warte 800ms befor wieder chooseword() aufgerufen wird
+    chooseword();
+  }, 700);
+}
+//Wenn Falscher Button gedrückt wird:
+function falseanswer(){
+  notactive=true;//Man kann nicht mehr antworten -> keine zweite Antwort möglich
+  document.getElementById("wrong").style.display="flex";//Falsch Banner einblenden
+  progress[wordIndexFull] = progress[wordIndexFull]+1;//Progress um 1 erhöhen
+  document.getElementById("rightword").textContent=enword;//richtiges Wort anzeigen
+  document.cookie=JSON.stringify(progress);
+  
+}
+
+//wird ausgeführt wenn submit Button gedrückt wird
+function submit(){
+  if (notactive == true){ //wenn man schon geantwortet hat, soll die funktion beendet werden.
+    return
+  }
+  eingabe = document.getElementById("eingabe").value //Eingabe aus Input abrufen
+  if (eingabe == enword){ //Wenn input gleich englisches Wort führe trueanswer() aus
+    trueanswer();
+  }
+  else{ //ansonsten führe falseanswer() aus
+    falseanswer();
+  }
+  document.getElementById("eingabe").value=""
+}
+
+//wird bei klick auf den jeweiligen button ausgeführt:
+function button1(){
+  while (notactive === true){//wenn man nicht Antorten kann->funktion beenden
+    return
+  }
+  if (ordre == 1){//wenn dieser Button die richtige Antwort ist:
+    console.log("true");
+    trueanswer();//Führe trueanswer() aus
+  }
+  else {//ansonsten:
+    console.log("false")
+    falseanswer();//führe falseanswer() aus.
+  }
+  return
+}
+
+function button2(){
+  if (notactive === true){
+    return
+  }
+  if (ordre == 2){
+    console.log("true");
+    trueanswer();
+  }
+  else {
+    console.log("false")
+    falseanswer();
+  }
+  return
+}
+
+function button3(){
+  if (notactive === true){
+    return
+  }
+  if (ordre == 3){
+    console.log("true");
+    trueanswer();
+  }
+  else {
+    console.log("false");
+    falseanswer();
+  }
+  return
+}
+
+function button4(){
+  if (notactive === true){
+    return
+  }
+  if (ordre == 4){
+    console.log("true");
+    trueanswer();
+  }
+  else {
+    console.log("false");
+    falseanswer();
+  }
+
+}
+
+
+if (document.cookie!=""){
+  progress = JSON.parse(document.cookie);
+  chooseword();
+}
+
+chooseword();
